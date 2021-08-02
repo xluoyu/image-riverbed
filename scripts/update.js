f0c05cf01cb27601a8af73100362354c42a211d6
萧萧落雨 <8071489+xluoyu@user.noreply.gitee.com>
2021-08-02 11:01:31 +0800

    update

--- scripts/update.js
+++ scripts/update.js
@@ -3,8 +3,10 @@ import git from 'simple-git'
 const githubPage = `https://xluoyu.github.io/image-riverbed/` 
 let diffArr = []
 
+console.log('\x1B[33m'+'代码运行中...'+'\x1B[0m')
 git()
 .status(['--ignored'], (err, val) => {
+  console.log('------ 新增的文件 ----------')
   console.log(val.not_added)
   diffArr = val.not_added
 })
@@ -13,8 +15,10 @@ git()
 .pull('origin', 'main')
 .push(['-f', 'origin', 'main'], () => {
   console.log('\x1B[32m'+'上传完成'+'\x1B[0m')
-  console.log('------ 以下是新增文件地址 ----------')
-  diffArr.forEach(url => {
-    console.log('\x1B[36m' + url + ': ' + githubPage + url + '\x1B[0m')
-  })
+  if (diffArr.length) {
+    console.log('------ 以下是新增文件地址 ----------')
+    diffArr.forEach(url => {
+      console.log('\x1B[36m' + url + ': ' + githubPage + url + '\x1B[0m')
+    })
+  }
 })
