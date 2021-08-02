0d6020f65d16274df791612a24c931ea97e3bcbe
萧萧落雨 <8071489+xluoyu@user.noreply.gitee.com>
2021-08-02 10:26:31 +0800

    update

--- scripts/update.js
+++ scripts/update.js
@@ -1,8 +1,18 @@
 import git from 'simple-git'
 
-git().add('.', (val) => {
-  console.log(val)
-}).commit('update').push(['-f', 'origin', 'main'], () => {
-  console.log('push to main success')
+const githubPage = `https://xluoyu.github.io/image-riverbed/` 
+let diffArr = []
 
+git().diffSummary((err, val) => {
+  diffArr = val.files.map(item => item.file)
+})
+.add('.')
+.commit('update')
+.pull('origin', 'main')
+.push(['-f', 'origin', 'main'], () => {
+  console.log('\x1B[32m', '上传完成')
+  console.log('------ 以下是最新文件地址 ----------')
+  diffArr.forEach(url => {
+    console.log('\x1B[36m', url + ': ' + githubPage + url)
+  })
 })
